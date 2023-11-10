639b9492acb9901499e38b67a4314bbdc813f6d2
ZixinYe123 <148769432+ZixinYe123@users.noreply.github.com>
2023-11-10 22:22:32 +0800

    Update edtion_3(1).py

--- edtion_3(1).py
+++ edtion_3(1).py
@@ -126,12 +126,12 @@ def query2():
   cols = ['Smoking', 'Alcohol use', 'Passive Smoker', 'Balanced Diet', 'Obesity','Weight Loss']
   selected_cols = st.multiselect('Select lifestyle factors', cols, default=cols)
 
-fig, ax = plt.subplots()
-for col in selected_cols:
-  labels = df_filtered[col].unique()
-  sizes = df_filtered[col].value_counts(normalize=True) * 100
-  ax.pie(sizes, labels=labels, autopct='%1.1f%%')
-st.pyplot(fig)
+  fig, ax = plt.subplots()
+  for col in selected_cols:
+    labels = df_filtered[col].unique()
+    sizes = df_filtered[col].value_counts(normalize=True) * 100
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%')
+  st.pyplot(fig)
 
   st.markdown("**Observations:**")
   if 'Smoking' in selected_cols:
