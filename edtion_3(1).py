364ad25e3fff3be2cf3c5a1bf12ca0f0104f5822
ZixinYe123 <148769432+ZixinYe123@users.noreply.github.com>
2023-11-10 22:15:11 +0800

    Update edtion_3(1).py

--- edtion_3(1).py
+++ edtion_3(1).py
@@ -126,10 +126,10 @@ def query2():
   cols = ['Smoking', 'Alcohol use', 'Passive Smoker', 'Balanced Diet', 'Obesity','Weight Loss']
   selected_cols = st.multiselect('Select lifestyle factors', cols, default=cols)
 
-  fig, ax = plt.subplots()
-  for col in selected_cols:
-      sns.violinplot(x = df_filtered[col], ax = ax)
-  st.pyplot(fig)
+df_filtered = df_filtered[selected_cols].reset_index(drop=True)
+fig, ax = plt.subplots()
+heatmap = sns.heatmap(df_filtered.astype(float), annot=True, fmt="g", ax=ax)
+st.write(heatmap)
 
   st.markdown("**Observations:**")
   if 'Smoking' in selected_cols:
